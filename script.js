1d20467e018ba90da6b9e77bc75ac821ddd944e3
814725104095-MohamedRiyaz <814725104095@trp.srmtrichy.edu.in>
2026-07-30 15:56:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,94 @@
-let team = [
+let team=[
+
 {name:"Rohit",runs:0},
 {name:"Gill",runs:0},
 {name:"Virat",runs:0},
 {name:"Rahul",runs:0},
 {name:"Hardik",runs:0}
+
 ];
 
-let batsman = 0;
-let total = 0;
-let history = [];
+let striker=0;
 
-function updateBoard(){
+let total=0;
 
-document.getElementById("totalScore").innerHTML = total;
+let wickets=0;
 
-let table = "";
+let balls=0;
+
+let history=[];
+
+function update(){
+
+document.getElementById("score").innerHTML=total;
+
+document.getElementById("wickets").innerHTML=wickets;
+
+let over=Math.floor(balls/6)+"."+(balls%6);
+
+document.getElementById("overs").innerHTML=over;
+
+let data="";
 
 for(let i=0;i<team.length;i++){
 
-table += `
+let status="";
+
+if(i==striker)
+status="🏏 Striker";
+
+data+=`
+
 <tr>
+
 <td>${team[i].name}</td>
+
 <td>${team[i].runs}</td>
+
+<td>${status}</td>
+
 </tr>
+
 `;
 
 }
 
-document.getElementById("playersTable").innerHTML = table;
+document.getElementById("players").innerHTML=data;
 
-if(history.length==0){
-document.getElementById("history").innerHTML="No Balls Yet";
-}
-else{
 document.getElementById("history").innerHTML=history.join(" ");
-}
 
 }
 
-function addBall(){
-
-let run = parseInt(document.getElementById("runs").value);
-
-if(isNaN(run) || run<0 || run>6){
-alert("Enter runs between 0 and 6");
-return;
-}
+function addBall(run){
 
 history.push(run);
 
-total += run;
+total+=run;
 
-team[batsman].runs += run;
+team[striker].runs+=run;
 
-if(run==0){
-batsman++;
-if(batsman>=team.length){
-batsman=team.length-1;
-}
-}
+balls++;
 
-document.getElementById("runs").value="";
-
-updateBoard();
+update();
 
 }
 
-updateBoard();
+function wicket(){
+
+history.push("W");
+
+wickets++;
+
+balls++;
+
+striker++;
+
+if(striker>=team.length)
+
+striker=team.length-1;
+
+update();
+
+}
+
+update();
